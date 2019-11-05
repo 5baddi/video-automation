f1b49415135cb21b5e5da1d77c5ad1200536be6d
5baddi <project@baddi.info>
2019-11-05 18:17:25 +0100

    Update cors origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,7 +8,7 @@ class Cors
 {
     /** List of allowed origin URLs */
     private $allowedOrigins = [
-        "https://dev14.v12dev.com/"
+        // "https://dev14.v12dev.com/"
     ];
 
     /**
@@ -20,15 +20,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        // Add headers to the response
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorization, X-Requested-With, X-Auth-Token');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response = $next($request);        
 
         // Set the allowed origin URL
-        if(in_array($request->server('HTTP_ORIGIN'), $this->allowedOrigins))
+        if(in_array($request->headers->get('origin'), $this->allowedOrigins)){
+            // Add headers to the response
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorization, X-Requested-With, X-Auth-Token');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
             $response->headers->set('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN'));
+        }
 
         return $response;
     }
