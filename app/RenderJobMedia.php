fdc30f215a3a4fe1e117a18170a78f1cbd6f6d52
5baddi <project@baddi.info>
2019-11-06 18:34:52 +0100

    Add video render medias history

---
+++ app/RenderJobMedia.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class RenderJobMedia extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = "va_render_job_medias";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'value'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'render_job_id' => 'integer',
+        'media_id'      => 'integer',
+    ];
+
+    /**
+     * Retrive parent object
+     *
+     * @return BelongsTo
+     */
+    public function media() : BelongsTo
+    {
+        return $this->belongsTo(TemplateMedia::class);
+    }
+
+    /**
+     * Retrive parent object
+     *
+     * @return BelongsTo
+     */
+    public function renderJob() : BelongsTo
+    {
+        return $this->belongsTo(RenderJob::class);
+    }
+}
--- database/migrations/2019_10_08_140238_videos_automation.php
+++ database/migrations/2019_10_08_140238_videos_automation.php
@@ -14,7 +14,7 @@ class VideosAutomation extends Migration
     public function up()
     {
         // Custom templates
-        Schema::create('custom_templates', function (Blueprint $table) {
+        Schema::create('va_custom_templates', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('vau_id')->nullable(false);
             $table->string('name')->unique()->nullable(false);
@@ -29,7 +29,7 @@ class VideosAutomation extends Migration
         });
 
         // Template medias
-        Schema::create('template_medias', function (Blueprint $table) {
+        Schema::create('va_template_medias', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('template_id');
             $table->unsignedBigInteger('scene')->nullable(false);
@@ -44,7 +44,7 @@ class VideosAutomation extends Migration
         });
 
         // Render jobs
-        Schema::create('render_jobs', function (Blueprint $table) {
+        Schema::create('va_render_jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('template_id');
             $table->unsignedBigInteger('vau_job_id')->unique()->nullable();
@@ -59,6 +59,15 @@ class VideosAutomation extends Migration
             $table->timestamps();
             $table->timestamp('finished_at')->nullable();
         });
+
+        // Render medias
+        Schema::create('va_render_job_medias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('render_job_id')->unqiue();
+            $table->unsignedBigInteger('media_id')->nullable(false);
+            $table->string('value')->nullable(false);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -68,8 +77,9 @@ class VideosAutomation extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('custom_templates');
-        Schema::dropIfExists('template_medias');
-        Schema::dropIfExists('render_jobs');
+        Schema::dropIfExists('va_custom_templates');
+        Schema::dropIfExists('va_template_medias');
+        Schema::dropIfExists('va_render_jobs');
+        Schema::dropIfExists('va_render_job_medias');
     }
 }
