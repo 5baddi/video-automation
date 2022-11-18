a1ebe09154faa316bd5d78f408ea422b42744c7d
Mohamed Baddi <project@baddi.info>
2022-11-18 20:08:13 +0100

    Update AutomationApp.php

--- app/AutomationApp.php
+++ app/AutomationApp.php
@@ -10,7 +10,7 @@ class AutomationApp
     const COMPANY_NAME = "V12 Software";
 
     const API_URL = "https://api.vau.company";
-    const ACCESS_TOKEN = "e61c55a57183d2fe29a3729de280b6e8076fe02c4c7f2e93924855b3fb2c64859db5fc6974e81b00390029b507b761b087ff4a5f93d6029cf8f2a511b6c97fac56baade3ef3328730c3056f99dac7952";
+    const ACCESS_TOKEN = "";
 
     const TEMPLATES_DIRECTORY_NAME = "va_templates";
     const OUTPUT_DIRECTORY_NAME = "va_outputs";
